be1b5d986804748de072f6b559ad5c26ac172669
584574838@qq.com <584574838@qq.com>
2021-11-22 22:21:20 +0800

    Update examples

--- examples/custom_button_events/main.c
+++ examples/custom_button_events/main.c
@@ -13,6 +13,18 @@
 
 // ...
 
+void onBtnPressed(fun_params_t params[])
+{
+  uint8_t btnId = (uint8_t)params[0].param.uint8Data;
+  printf("[%0.8d] Btn%x pressed\r\n", el_getMillis(), btnId);
+}
+
+void onBtnReleased(fun_params_t params[])
+{
+  uint8_t btnId = (uint8_t)params[0].param.uint8Data;
+  printf("[%0.8d] Btn%x released\r\n", el_getMillis(), btnId);
+}
+
 void helloWorld() {
   printf("[%0.8d] hello world\r\n", Sys_GetMillis());
 }
@@ -20,18 +32,18 @@ void helloWorld() {
 void startTestTasks()
 {
   fun_params_t *blinkParams = (fun_params_t *)malloc(sizeof(fun_params_t) * 2);
-  blinkParams[0].param.int32Data = 50;
+  blinkParams[0].param.int32Data = 20;
   blinkParams[1].param.int32Data = 0;
   // 周期任务：每隔 2000ms LED_R 点亮 50ms
-  FW_SetInterval(LedR_Blink, blinkParams, 2000, IMMEDIATE_N);
+  el_setInterval(LedR_Blink, blinkParams, 2000, IMMEDIATE_N);
   // 一次性任务：在下一个 tick 打印出 hello world
-  FW_NextTick(helloWorld, NULL);
+  el_nextTick(helloWorld, NULL);
   // 周期任务：每 10ms 执行 observBtnState 函数观察按钮状态
-  FW_SetInterval(observBtnState, NULL, 10, IMMEDIATE_Y);
+  el_setInterval(observBtnState, NULL, 10, IMMEDIATE_Y);
 
   // 监听按钮 press 和 release 事件
-  ET_AddEventListener(ET_BTN_PRESS, onBtnPressed);
-  ET_AddEventListener(ET_BTN_RELEASE, onBtnReleased);
+  el_addEventListener(EVENT_BTN_PRESS, onBtnPressed);
+  el_addEventListener(EVENT_BTN_RELEASE, onBtnReleased);
 }
 /* USER CODE END 0 */
 
@@ -41,6 +53,6 @@ int main(void)
 {
   // ...
   // 启动任务，不设置帧率，将 startTestTasks 作为入口函数，按最高频率进行事件循环
-  FW_Start(startTestTasks, NULL);
+  el_startLoop(startTestTasks, NULL);
   // ...
 }
