55c7951d2fcb3bc694d61da229d328c8e98a78dd
584574838@qq.com <584574838@qq.com>
2021-11-27 17:52:32 +0800

    补充基本使用示例

---
+++ examples/base/setInterval.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+#include "eos.h"
+
+void echoCounter()
+{
+  static uint16_t count = 0;
+  printf("Count: %d\r\n", count++);
+}
+
+int main(void)
+{
+  // ...
+  /**
+   * @brief 每隔 1 秒执行一次 echoCounter 函数
+   * 
+   * 依次输出：
+   * Count: 0
+   * Count: 1
+   * Count: 2
+   * ...
+   */
+  el_setInerval(echoCounter, 1000, NULL, IMMEDIATE_N);
+  // 启动事件循环
+  el_startLoop();
+  // ...
+}
---
+++ examples/base/setTimeout.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+#include "eos.h"
+
+void helloWorld()
+{
+  printf("hello world!\r\n");
+}
+
+int main(void)
+{
+  // ...
+  /**
+   * @brief 一秒钟后执行 helloWorld 函数
+   * 
+   * 输出：
+   * hello world!
+   */
+  el_setTimeout(helloWorld, 1000, NULL);
+  // 启动事件循环
+  el_startLoop(startTestTasks);
+  // ...
+}
